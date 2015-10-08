7a2b3b391887fcc0b0849ed3b00f88ce15530b48
HE Boliang <hebl@hebl.org>
2015-10-08 13:53:53 +0800

    bugfix: tab to blank

--- Shell/rsync/rsync_repo.sh
+++ Shell/rsync/rsync_repo.sh
@@ -11,33 +11,33 @@ rsyncBin="/usr/bin/rsync"
 
 function rsync_yum_repo()
 {
-	repo_name=$1
+    repo_name=$1
 
-	# build rsync paramters
-	case $repo_name in
-		centos)
-			logFile="/data/log/rsync_yum/centos_$Date.log"
-			excludeFile="/archive/yum.repos/centosExclude.list"
-			rsyncParameters="-avrtL --delete --exclude-from $excludeFile"
-			mirrorUrl="rsync://mirrors.ustc.edu.cn/centos/"
-			localSyncPath="/archive/yum.repos/centos/"
-		;;
-		epel)
-			logFile="/data/log/rsync_yum/epel_$Date.log"
-			excludeFile="/archive/yum.repos/epelExclude.list"
-			rsyncParameters="-avrtL --delete --exclude-from $excludeFile"
-			mirrorUrl="rsync://mirrors.ustc.edu.cn/epel/"
-			localSyncPath="/archive/yum.repos/epel/"
-		;;
-	esac
+    # build rsync paramters
+    case $repo_name in
+        centos)
+            logFile="/data/log/rsync_yum/centos_$Date.log"
+            excludeFile="/archive/yum.repos/centosExclude.list"
+            rsyncParameters="-avrtL --delete --exclude-from $excludeFile"
+            mirrorUrl="rsync://mirrors.ustc.edu.cn/centos/"
+            localSyncPath="/archive/yum.repos/centos/"
+        ;;
+        epel)
+            logFile="/data/log/rsync_yum/epel_$Date.log"
+            excludeFile="/archive/yum.repos/epelExclude.list"
+            rsyncParameters="-avrtL --delete --exclude-from $excludeFile"
+            mirrorUrl="rsync://mirrors.ustc.edu.cn/epel/"
+            localSyncPath="/archive/yum.repos/epel/"
+        ;;
+    esac
 
-	# begin to sync
-	echo "---- $Date `date +%T` Begin ----" >> $logFile
-	$rsyncBin $mirrorUrl $rsyncParameters $localSyncPath >> $logFile 2>&1
-	echo "---- $Date `date +%T` End ----" >> $logFile
+    # begin to sync
+    echo "---- $Date `date +%T` Begin ----" >> $logFile
+    $rsyncBin $mirrorUrl $rsyncParameters $localSyncPath >> $logFile 2>&1
+    echo "---- $Date `date +%T` End ----" >> $logFile
 }
 
 for repo in centos epel
 do
-	rsync_yum_repo $repo
+    rsync_yum_repo $repo
 done
